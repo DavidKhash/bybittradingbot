56bd282133e5d1c21d99280e784c1f765f4f39be
DavidKhash <davidkhash11@gmail.com>
2025-02-11 01:59:33 -0500

    changed name of dir in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
    #!/bin/bash
    # Navigate to the project folder (adjust the path if needed)
-   cd ~/crypto-search || exit
+   cd ~/bybittradingbot || exit
    # Pull on the latest changes from GitHub
    git pull origin main
    # Or if not using PM2, you might run:
